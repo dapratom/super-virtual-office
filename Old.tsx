f1f25def7ab407699050eabfeb87cd48eab397a2
dapratom <78089139+dapratom@users.noreply.github.com>
2021-02-04 18:42:59 +0000

    Deploying to gh-pages from master @ 52ffc02636e72a0f7849531b47a5c0addf04c2fc 🚀

--- Old.tsx
+++
@@ -1,7 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="Old" tilewidth="1024" tileheight="2016" tilecount="1" columns="0">
- <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="0">
-  <image width="1024" height="2016" source="tilesetold.png"/>
- </tile>
-</tileset>
